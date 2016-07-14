688863490e93b4809c81c7d2511cfffd18205d6e
alexstocks <dragoncharlie@foxmail.com>
2016-07-14 14:26:44 +0800

    add xorlist

---
+++ src/container/xorlist/example_test.go
@@ -0,0 +1,30 @@
+// Copyright 2015 AlexStocks(https://github.com/AlexStocks).
+// All rights reserved.  Use of this source code is
+// governed by a BSD-style license.
+
+package xorlist_test
+
+import (
+	"container/xorlist"
+	"fmt"
+)
+
+func Example() {
+	// Create a new list and put some numbers in it.
+	l := xorlist.New()
+	e4 := l.PushBack(4)
+	e1 := l.PushFront(1)
+	l.InsertBefore(3, e4)
+	l.InsertAfter(2, e1)
+
+	// Iterate through list and print its contents.
+	for e, p := l.Front(); e != nil; e, p = e.Next(p), e {
+		fmt.Println(e.Value)
+	}
+
+	// Output:
+	// 1
+	// 2
+	// 3
+	// 4
+}
