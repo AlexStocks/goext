cc5b7b6aa95e97af8c17bace7b10b3232af22c78
alexstocks <dragoncharlie@foxmail.com>
2016-09-22 17:53:19 +0800

    add multiple loggers test

--- log/log_test.go
+++ log/log_test.go
@@ -27,7 +27,6 @@ func TestNewLogger(t *testing.T) {
 }
 
 func TestNewLoggerWithConfFile(t *testing.T) {
-
 	var (
 		err    error
 		dir    string
@@ -49,4 +48,36 @@ func TestNewLoggerWithConfFile(t *testing.T) {
 	logger.Critic("Critic")
 
 	logger.Close()
+}
+
+func TestMultiLoggers(t *testing.T) {
+	var conf = Conf{Name: "test1", Dir: "./log/", Level: "Info", Console: false, Daily: true, BackupNum: 2}
+
+	var (
+		err     error
+		logger1 Logger
+		logger2 Logger
+	)
+	if logger1, err = NewLogger(conf); err != nil {
+		t.Errorf("NewLogger(conf{%#v}) = error{%#v}", conf, err)
+	}
+	conf = Conf{Name: "test2", Dir: "./log/", Level: "Info", Console: false, Daily: true, BackupNum: 2}
+	if logger2, err = NewLogger(conf); err != nil {
+		t.Errorf("NewLogger(conf{%#v}) = error{%#v}", conf, err)
+	}
+
+	logger1.Debug("Debug")
+	logger1.Info("Info")
+	logger1.Warn("Warn")
+	logger1.Error("Error")
+	logger1.Critic("Critic")
+
+	logger2.Debug("Debug")
+	logger2.Info("Info")
+	logger2.Warn("Warn")
+	logger2.Error("Error")
+	logger2.Critic("Critic")
+
+	logger2.Close()
+	logger1.Close()
 }
