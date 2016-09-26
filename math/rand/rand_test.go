9133a99eaf6561e31863c9065e12e7ffb4e1af41
alexstocks <dragoncharlie@foxmail.com>
2016-09-26 14:45:40 +0800

    add uuid

--- math/rand/rand_test.go
+++ math/rand/rand_test.go
@@ -11,3 +11,7 @@ func TestRandString(t *testing.T) {
 	fmt.Println(RandString(4))
 	fmt.Println(RandDigitString(4))
 }
+
+func TestNewUUID(t *testing.T) {
+	fmt.Println(NewUUID())
+}
---
+++ math/rand/uuid.go
@@ -0,0 +1,36 @@
+// Copyright 2016 AlexStocks(https://github.com/AlexStocks).
+// All rights reserved.  Use of this source code is
+// governed by a BSD-style license.
+
+// Package gxrand encapsulates some golang.math.rand functions
+package gxrand
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+)
+
+// refer to https://github.com/gorilla/feeds/blob/master/uuid.go
+type UUID [16]byte
+
+// create a new uuid v4
+func NewUUID() *UUID {
+	u := &UUID{}
+	_, err := rand.Read(u[:16])
+	if err != nil {
+		panic(err)
+	}
+
+	u[8] = (u[8] | 0x80) & 0xBf
+	u[6] = (u[6] | 0x40) & 0x4f
+	return u
+}
+
+func (u *UUID) String() string {
+	return fmt.Sprintf("%x-%x-%x-%x-%x", u[:4], u[4:6], u[6:8], u[8:10], u[10:])
+}
+
+func (u *UUID) HexString() string {
+	return hex.EncodeToString(u[:16])
+}
