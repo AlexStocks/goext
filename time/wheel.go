305b97325b1c5f6a453829773f2b2b746f9b4689
alexstocks <dragoncharlie@foxmail.com>
2016-09-26 16:13:56 +0800

    add time wheel

---
+++ time/wheel.go
@@ -0,0 +1,98 @@
+// Copyright 2016 AlexStocks(https://github.com/AlexStocks).
+// All rights reserved.  Use of this source code is
+// governed by a BSD-style license.
+
+// Package gxtime encapsulates some golang.time functions
+package gxtime
+
+// refer to https://github.com/siddontang/go/blob/master/timingring/timingring.go
+
+import (
+	"sync"
+	"time"
+)
+
+type empty struct{}
+
+type Wheel struct {
+	sync.Mutex
+	span   time.Duration
+	period time.Duration
+	ticker *time.Ticker
+	index  int
+	ring   []chan empty
+	done   chan empty
+	once   sync.Once
+}
+
+func NewWheel(span time.Duration, buckets int) *Wheel {
+	var (
+		this *Wheel
+	)
+
+	if span == 0 {
+		panic("@span == 0")
+	}
+	if buckets == 0 {
+		panic("@bucket == 0")
+	}
+
+	this = &Wheel{
+		span:   span,
+		period: span * (time.Duration(buckets)),
+		ticker: time.NewTicker(span),
+		index:  0,
+		ring:   make([](chan empty), buckets),
+		done:   make(chan empty),
+	}
+
+	for idx := range this.ring {
+		this.ring[idx] = make(chan empty)
+	}
+
+	go func() {
+		var notify chan empty
+		for {
+			select {
+			case <-this.ticker.C:
+				this.Lock()
+
+				notify = this.ring[this.index]
+				this.ring[this.index] = make(chan empty)
+				this.index = (this.index + 1) % len(this.ring)
+
+				this.Unlock()
+
+				close(notify)
+			case <-this.done:
+				this.ticker.Stop()
+				return
+			}
+		}
+	}()
+
+	return this
+}
+
+func (this *Wheel) Stop() {
+	select {
+	case <-this.done:
+		// this.done is a blocked channel. if it has not been closed, the default branch will be invoked.
+		return
+
+	default:
+		this.once.Do(func() { close(this.done) })
+	}
+}
+
+func (this *Wheel) After(timeout time.Duration) <-chan empty {
+	if timeout >= this.period {
+		panic("@timeout over ring's life period")
+	}
+
+	this.Lock()
+	c := this.ring[(this.index+int(timeout/this.span))%len(this.ring)]
+	this.Unlock()
+
+	return c
+}
---
+++ time/wheel_test.go
@@ -0,0 +1,26 @@
+package gxtime
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestNewWheel(t *testing.T) {
+	var (
+		index int
+		wheel *Wheel
+	)
+	wheel = NewWheel(TimeMillisecondDuration(100), 20)
+	defer wheel.Stop()
+
+	for {
+		select {
+		case <-wheel.After(TimeMillisecondDuration(1000)):
+			fmt.Println("loop:", index)
+			index++
+			if index > 10 {
+				return
+			}
+		}
+	}
+}
