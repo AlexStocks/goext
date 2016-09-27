51105b61886b946cda89615b543f7c3016a9cbfe
alexstocks <dragoncharlie@foxmail.com>
2016-09-27 15:13:12 +0800

    add String&Slice in strings

---
+++ strings/slice.go
@@ -0,0 +1,26 @@
+// http://blog.csdn.net/siddontang/article/details/23541587
+// vitess代码，一种很hack的做法，string和slice的转换只需要拷贝底层的指针，而不是内存拷贝。
+
+package gxstrings
+
+import (
+	"reflect"
+	"unsafe"
+)
+
+func String(b []byte) (s string) {
+	pbytes := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	pstring := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	pstring.Data = pbytes.Data
+	pstring.Len = pbytes.Len
+	return
+}
+
+func Slice(s string) (b []byte) {
+	pbytes := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	pstring := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	pbytes.Data = pstring.Data
+	pbytes.Len = pstring.Len
+	pbytes.Cap = pstring.Len
+	return
+}
