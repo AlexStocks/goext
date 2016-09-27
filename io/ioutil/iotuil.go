69e09fb08d272978786bf2da9ad2cdc8c3902487
alexstocks <dragoncharlie@foxmail.com>
2016-09-27 14:47:49 +0800

    add uuid & readtxt

---
+++ io/ioutil/iotuil.go
@@ -0,0 +1,37 @@
+// Copyright 2016 AlexStocks(https://github.com/AlexStocks).
+// All rights reserved.  Use of this source code is
+// governed by a BSD-style license.
+
+// Package gxioutil implements some I/O utility functions.
+package gxioutil
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func ReadTxt(file string) ([]byte, error) {
+	if len(file) == 0 {
+		return nil, errors.New("@file is nil")
+	}
+
+	if _, err := os.Stat(file); os.IsNotExist(err) {
+		return nil, fmt.Errorf("@file{%s} not found", file)
+	}
+	f, err := os.OpenFile(file, os.O_RDONLY, 0666)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	reader := bufio.NewReader(f)
+	data, err := ioutil.ReadAll(reader)
+	if len(data) == 0 {
+		return nil, fmt.Errorf("can not get content of @file{%s}", file)
+	}
+
+	return data, err
+}
--- strings/uuid.go
+++ strings/uuid.go
@@ -1,3 +1,10 @@
+// Copyright 2016 AlexStocks(https://github.com/AlexStocks).
+// All rights reserved.  Use of this source code is
+// governed by a BSD-style license.
+
+// Package gxstrings implements string related utilities.
+
+package gxstrings
 
 // refer to https://github.com/gorilla/feeds/blob/master/uuid.go
 
