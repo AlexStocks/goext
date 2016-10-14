884b0d0cc7c9905d99a742bfa0954f9834ae6a6a
alexstocks <dragoncharlie@foxmail.com>
2016-10-14 19:52:42 +0800

    add goext/net/WSHostAddress/WSSHostAddress/WSHostAddress2/WSSHostAddress2

--- net/net.go
+++ net/net.go
@@ -15,8 +15,24 @@ func HostAddress(host string, port int) string {
 	return net.JoinHostPort(host, strconv.Itoa(port))
 }
 
+func WSHostAddress(host string, port int, path string) string {
+	return "ws://" + net.JoinHostPort(host, strconv.Itoa(port)) + path
+}
+
+func WSSHostAddress(host string, port int, path string) string {
+	return "wss://" + net.JoinHostPort(host, strconv.Itoa(port)) + path
+}
+
 func HostAddress2(host string, port string) string {
 	return net.JoinHostPort(host, port)
+}
+
+func WSHostAddress2(host string, port string) string {
+	return "ws://" + net.JoinHostPort(host, port) + path
+}
+
+func WSSHostAddress2(host string, port string) string {
+	return "wss://" + net.JoinHostPort(host, port) + path
 }
 
 func HostPort(addr string) (string, string, error) {
