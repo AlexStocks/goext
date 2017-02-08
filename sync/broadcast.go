77a91934a03b83005b43b982e9e4f41bd56ae10c
AlexStocks <alexstocks@foxmail.com>
2017-02-08 15:21:26 +0800

    add select func for reader

--- sync/broadcast.go
+++ sync/broadcast.go
@@ -1,4 +1,4 @@
-// Copyright 2016 AlexStocks(https://github.com/AlexStocks).
+// Copyright 2016 AlexStocks(https://.com/AlexStocks).
 // All rights reserved.  Use of this source code is
 // governed by a BSD-style license.
 
@@ -12,51 +12,44 @@ var (
 	ErrBroadcastClosed = fmt.Errorf("broadcast closed!")
 )
 
-type node struct {
-	next  chan node
+type Node struct {
+	next  chan Node
 	value interface{}
 }
 
 type Broadcaster struct {
-	registry chan chan (chan node)
+	registry chan chan (chan Node)
 	data     chan<- interface{}
 }
 
 type Receiver struct {
-	R chan node
+	C chan Node
 }
 
 // create a new broadcaster object.
 func NewBroadcaster() Broadcaster {
-	registry := make(chan (chan (chan node)))
+	registry := make(chan (chan (chan Node)))
 	data := make(chan interface{})
 	go func() {
 		var (
-			value    interface{}
-			cursor   chan node
-			tmp_node chan node
-			reader   chan chan node
+			value  interface{}
+			cursor chan Node
+			node   chan Node
+			reader chan chan Node
 		)
-		cursor = make(chan node, 1)
-		// gxlog.CError("first cursor:%#value\n", cursor)
+		cursor = make(chan Node, 1)
 		for {
 			select {
 			case value = <-data:
 				if value == nil {
-					cursor <- node{}
-					// gxlog.CError("last cursor:%#value\n", cursor)
+					cursor <- Node{}
 					return
 				}
-				tmp_node = make(chan node, 1)
-				// b := node{next: node, value: value}
-				// gxlog.CError("value:%#value, b:%#value, node:%#value, cursor:%#value\n", value, b, node, cursor)
-				// cursor <- b
-				cursor <- node{next: tmp_node, value: value}
-				cursor = tmp_node
-				// gxlog.CError("cursor:%#value\n", cursor)
+				node = make(chan Node, 1)
+				cursor <- Node{next: node, value: value}
+				cursor = node
 			case reader = <-registry:
 				reader <- cursor
-				// gxlog.CError("new reader:%#value", reader)
 			}
 		}
 	}()
@@ -68,13 +61,12 @@ func NewBroadcaster() Broadcaster {
 
 // start listening to the nodes.
 func (b Broadcaster) Listen() Receiver {
-	r := make(chan chan node, 0)
-	// gxlog.CError("new reader channel:%#value", node)
+	r := make(chan chan Node, 0)
 	b.registry <- r
 	return Receiver{<-r}
 }
 
-// node a value to all listeners.
+// Node a value to all listeners.
 func (b Broadcaster) Write(value interface{}) (rerr error) {
 	defer func() {
 		if e := recover(); e != nil {
@@ -96,16 +88,29 @@ func (b Broadcaster) Close() { b.Write(nil) }
 // read a value that has been broadcast,
 // waiting until one is available if necessary.
 func (r *Receiver) Read() (interface{}, error) {
-	b := <-r.R
-	value := b.value
-	r.R <- b
-	// gxlog.CDebug("b:%#value, r:%#value", b, r)
-	r.R = b.next
-	// gxlog.CDebug("new r:%#value", r)
+	n := <-r.C
+	value := n.value
+	r.C <- n
+	r.C = n.next
 
 	if value == nil {
 		return nil, ErrBroadcastClosed
 	}
 
 	return value, nil
+}
+
+func (r *Receiver) ReadChan() chan Node {
+	return r.C
+}
+
+func (r *Receiver) ReadDone(n Node) (interface{}, error) {
+	r.C <- n
+	r.C = n.next
+
+	if n.value == nil {
+		return nil, ErrBroadcastClosed
+	}
+
+	return n.value, nil
 }
--- sync/broadcast_test.go
+++ sync/broadcast_test.go
@@ -7,11 +7,9 @@ import (
 // go test -v -run TestBroadcast
 func TestBroadcast(t *testing.T) {
 	b := NewBroadcaster()
-	// gxlog.CError("nodeer:%#value", b)
 
 	// case 1 : one reader
 	r := b.Listen()
-	// gxlog.CError("listen reader:%#value\n", r)
 
 	b.Write("hello")
 
@@ -21,16 +19,15 @@ func TestBroadcast(t *testing.T) {
 
 	// case 2: two readers
 	r1 := b.Listen()
-	// gxlog.CError("listen reader:%#value\n", r1)
 
 	b.Write(123)
 
 	if v, _ := r.Read(); v.(int) != 123 {
-		t.Fatal("error int")
+		t.Fatalf("error read value:%#v", v)
 	}
 
 	if v, _ := r1.Read(); v.(int) != 123 {
-		t.Fatal("error int")
+		t.Fatalf("error read value:%#v", v)
 	}
 
 	// case 3: close
@@ -47,5 +44,22 @@ func TestBroadcast(t *testing.T) {
 	// cse 4: write after close
 	if e := b.Write(123); e.Error() != "panic error:send on closed channel" {
 		t.Fatalf("writer error:%v", e)
+	}
+}
+
+// go test -v -run TestReceiver_DoneRead
+func TestReceiver_DoneRead(t *testing.T) {
+	b := NewBroadcaster()
+	r := b.Listen()
+	b.Write(123)
+
+	var v interface{}
+	select {
+	case n := <-r.ReadChan():
+		v, _ = r.ReadDone(n)
+	}
+
+	if v.(int) != int(123) {
+		t.Fatalf("error read value:%#v", v)
 	}
 }
