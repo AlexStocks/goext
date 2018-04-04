993f4aed383fded8c6ffb78da626b3eeaebe53fa
AlexStocks <alexstocks@foxmail.com>
2018-04-04 22:25:17 +0800

    add WriteFile, ReadTxt -> ReadFile

--- io/ioutil/iotuil.go
+++ io/ioutil/iotuil.go
@@ -1,4 +1,4 @@
-// Copyright 2016 AlexStocks(https://github.com/AlexStocks).
+// Copyright 2016 ~ 2018 AlexStocks(https://github.com/AlexStocks).
 // All rights reserved.  Use of this source code is
 // governed by Apache License 2.0.
 
@@ -7,31 +7,37 @@ package gxioutil
 
 import (
 	"bufio"
-	"errors"
-	"fmt"
 	"io/ioutil"
 	"os"
 )
 
-func ReadTxt(file string) ([]byte, error) {
+import (
+	"github.com/juju/errors"
+)
+
+func ReadFile(file string) ([]byte, error) {
 	if len(file) == 0 {
-		return nil, errors.New("@file is nil")
+		return nil, errors.Errorf("@file is nil")
 	}
 
 	if _, err := os.Stat(file); os.IsNotExist(err) {
-		return nil, fmt.Errorf("@file{%s} not found", file)
+		return nil, errors.Errorf("@file{%s} not found", file)
 	}
 	f, err := os.OpenFile(file, os.O_RDONLY, 0666)
 	if err != nil {
-		return nil, err
+		return nil, errors.Annotatef(err, "file:%s", file)
 	}
 	defer f.Close()
 
 	reader := bufio.NewReader(f)
 	data, err := ioutil.ReadAll(reader)
 	if len(data) == 0 {
-		return nil, fmt.Errorf("can not get content of @file{%s}", file)
+		return nil, errors.Errorf("can not get content of @file{%s}", file)
 	}
 
-	return data, err
+	return data, nil
+}
+
+func WriteFile(file string, data []byte) error {
+	return ioutil.WriteFile(file, data, 0644)
 }
