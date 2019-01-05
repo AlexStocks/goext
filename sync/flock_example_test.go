acb9243c069a9a2f29f64e893733d6f5ef9f76d7
AlexStocks <alexstocks@foxmail.com>
2019-01-05 16:52:23 +0800

    Add: file lock

---
+++ sync/flock_example_test.go
@@ -0,0 +1,76 @@
+// Copyright 2015 Tim Heckman. All rights reserved.
+// Copyright 2018 The Gofrs. All rights reserved.
+// Use of this source code is governed by the BSD 3-Clause
+// license that can be found in the FLOCK_LICENSE file.
+
+// Copyright 2016 ~ 2018 AlexStocks(https://github.com/AlexStocks).
+// All rights reserved.  Use of this source code is
+// governed by Apache License 2.0.
+//
+// refers to github.com/gofrs/flock
+
+package gxsync
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+)
+
+func ExampleFlock_Locked() {
+	f := NewFlock(os.TempDir() + "/go-lock.lock")
+	f.TryLock() // unchecked errors here
+
+	fmt.Printf("locked: %v\n", f.Locked())
+
+	f.Unlock()
+
+	fmt.Printf("locked: %v\n", f.Locked())
+	// Output: locked: true
+	// locked: false
+}
+
+func ExampleFlock_TryLock() {
+	// should probably put these in /var/lock
+	fileLock := NewFlock(os.TempDir() + "/go-lock.lock")
+
+	locked, err := fileLock.TryLock()
+
+	if err != nil {
+		// handle locking error
+	}
+
+	if locked {
+		fmt.Printf("path: %s; locked: %v\n", fileLock.Path(), fileLock.Locked())
+
+		if err := fileLock.Unlock(); err != nil {
+			// handle unlock error
+		}
+	}
+
+	fmt.Printf("path: %s; locked: %v\n", fileLock.Path(), fileLock.Locked())
+}
+
+func ExampleFlock_TryLockContext() {
+	// should probably put these in /var/lock
+	fileLock := NewFlock(os.TempDir() + "/go-lock.lock")
+
+	lockCtx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	locked, err := fileLock.TryLockContext(lockCtx, 678*time.Millisecond)
+
+	if err != nil {
+		// handle locking error
+	}
+
+	if locked {
+		fmt.Printf("path: %s; locked: %v\n", fileLock.Path(), fileLock.Locked())
+
+		if err := fileLock.Unlock(); err != nil {
+			// handle unlock error
+		}
+	}
+
+	fmt.Printf("path: %s; locked: %v\n", fileLock.Path(), fileLock.Locked())
+}
